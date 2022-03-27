3f6bc4d11cda12bb172a1327b2a87bbb32f6de6a
karokarom <101216521+karokarom@users.noreply.github.com>
2022-03-27 22:10:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -181,4 +181,4 @@ client.on("%play", () => {
 // url: "https://www.twitch.tv/nocopyrightsounds"})
 // console.log(`Successfully logined as ${client.user.tag}});
 
-client.login("OTU0MDI4MjAxMTQxNzUxODE4.YjNKAA.ER1ULVHrNLAbPEVitI4f94p9m-E");
+client.login("OTU0MDI4MjAxMTQxNzUxODE4.YjNKAA.zgim4NMrLmFVl7EdSfhe9AH7DXE");
